ad453f6a27a7dc3dcb7a616c20cea32640046829
Eugene <smilefox2806@gmail.com>
2024-03-08 15:53:32 +0700

    Начало работы в Go, Переменные(Дополнено)

--- Lesson1/src/task.go
+++ Lesson1/src/task.go
@@ -4,17 +4,26 @@ import (
 	"fmt"
 )
 
-func main() {
+func validate() int {
 	var (
 		a int
 		b int
 	)
-	fmt.Scan(&a, &b)
-
-	if a+b < 100 && a > 0 && b > 0 {
-		fmt.Println("Сумма чисел", a+b)
-	} else {
+	fmt.Println("Введите первое число")
+	_, err := fmt.Scan(&a)
+	fmt.Println("Введите второе число")
+	_, err2 := fmt.Scan(&b)
+	if err != nil && err2 != nil {
 		fmt.Println("Error")
+		return 0
 	}
+	if a+b > 100 || a < 0 || b < 0 {
+		fmt.Println("Error")
+		return 0
+	}
+	return a + b
+}
+func main() {
+	fmt.Println("Сумма чисел", validate())
 
 }
