e778161519879a1eb3b350c7d9ed33e9676bfeeb
Eugene <smilefox2806@gmail.com>
2024-05-08 17:28:17 +0700

    Переписал тесты питекантропа на человеческие

--- store/internal/model/model_test.go
+++ store/internal/model/model_test.go
@@ -7,129 +7,145 @@ import (
 
 func TestAddItem(t *testing.T) {
 	//1.
-	actualCart := Cart{Items: make(map[string]CardItem, 3)}
 
-	productsForAdd := []*Item{
-		{"Milk", 1000, 70},
-		{"Bread", 200, 40},
-		{"Snack", 100, 50},
+	testCases := []struct {
+		name       string
+		before     Cart
+		insertItem Item
+		expected   Cart
+	}{
+		{name: "test1",
+			before: Cart{make(map[string]CardItem)},
+
+			insertItem: Item{
+				name:     "Milk",
+				weight:   900,
+				priceRUB: 70,
+			},
+
+			expected: Cart{map[string]CardItem{
+				"Milk": {
+					item:      &Item{"Milk", 900, 70},
+					weightSum: 900,
+					priceSum:  70,
+					countItem: 1,
+				},
+			},
+			},
+		},
+
+		{name: "test2",
+			before: Cart{map[string]CardItem{
+				"Bread": {
+					item:      &Item{"Bread", 200, 40},
+					weightSum: 200,
+					priceSum:  40,
+					countItem: 1,
+				},
+			},
+			},
+
+			insertItem: Item{
+				name:     "Bread",
+				weight:   200,
+				priceRUB: 40,
+			},
+
+			expected: Cart{map[string]CardItem{
+				"Bread": {
+					item:      &Item{"Bread", 200, 40},
+					weightSum: 400,
+					priceSum:  80,
+					countItem: 2,
+				},
+			},
+			},
+		},
 	}
 
-	for _, product := range productsForAdd {
-		actualCart.Items[product.name] = CardItem{
-			item:      product,
-			weightSum: product.weight,
-			priceSum:  product.priceRUB,
-			countItem: 1,
-		}
+	for _, testCase := range testCases {
+		testCase.before.AddItem(&testCase.insertItem)
+
+		assert.Equal(t, testCase.expected, testCase.before)
 	}
-
-	expectedCart := Cart{Items: make(map[string]CardItem, 3)}
-
-	for _, product := range productsForAdd {
-		expectedCart.AddItem(product)
-	}
-
-	assert.Equal(t, actualCart, expectedCart)
-
-	//2.
-	actualCart2 := Cart{Items: make(map[string]CardItem, 3)}
-
-	productsForAddActualCart2 := []*Item{
-		{"Milk", 1000, 70},
-		{"Bread", 200, 40},
-		{"Snack", 100, 50},
-	}
-
-	for _, product := range productsForAddActualCart2 {
-		actualCart2.Items[product.name] = CardItem{
-			item:      product,
-			weightSum: product.weight * 2,
-			priceSum:  product.priceRUB * 2,
-			countItem: 2,
-		}
-	}
-
-	productsForAddExpectedCart2 := []*Item{
-		{"Milk", 1000, 70},
-		{"Bread", 200, 40},
-		{"Snack", 100, 50},
-		{"Milk", 1000, 70},
-		{"Bread", 200, 40},
-		{"Snack", 100, 50},
-	}
-
-	expectedCart2 := Cart{Items: make(map[string]CardItem, 3)}
-
-	for _, product := range productsForAddExpectedCart2 {
-		expectedCart2.AddItem(product)
-	}
-
-	assert.Equal(t, actualCart2, expectedCart2)
 }
 
 func TestRemoveItem(t *testing.T) {
+	testCases := []struct {
+		name       string
+		before     Cart
+		removeItem Item
+		expected   Cart
+	}{
+		{name: "test1",
 
-	//Подготовка
-	cart := Cart{Items: make(map[string]CardItem, 1)}
-	productForDelete := Item{
-		name:     "Milk",
-		weight:   1000,
-		priceRUB: 70,
+			before: Cart{make(map[string]CardItem)},
+
+			removeItem: Item{
+				name:     "Milk",
+				weight:   900,
+				priceRUB: 70,
+			},
+
+			expected: Cart{make(map[string]CardItem)},
+		},
+		{name: "test2",
+
+			before: Cart{map[string]CardItem{
+
+				"Milk": {
+					item:      &Item{"Milk", 900, 70},
+					weightSum: 900,
+					priceSum:  70,
+					countItem: 1,
+				},
+			},
+			},
+
+			removeItem: Item{
+				name:     "Milk",
+				weight:   900,
+				priceRUB: 70,
+			},
+
+			expected: Cart{make(map[string]CardItem)},
+		},
+
+		{name: "test3",
+
+			before: Cart{map[string]CardItem{
+
+				"Milk": {
+					item:      &Item{"Milk", 900, 70},
+					weightSum: 1800,
+					priceSum:  140,
+					countItem: 2,
+				},
+			},
+			},
+
+			removeItem: Item{
+				name:     "Milk",
+				weight:   900,
+				priceRUB: 70,
+			},
+
+			expected: Cart{map[string]CardItem{
+
+				"Milk": {
+					item:      &Item{"Milk", 900, 70},
+					weightSum: 900,
+					priceSum:  70,
+					countItem: 1,
+				},
+			},
+			},
+		},
 	}
-	cart.Items["Milk"] = CardItem{
-		item:      &productForDelete,
-		weightSum: 0,
-		priceSum:  0,
-		countItem: 0,
+
+	for _, testCase := range testCases {
+		testCase.before.RemoveItem(&testCase.removeItem)
+
+		assert.Equal(t, testCase.expected, testCase.before)
 	}
-	//Выполнение
-	cart.RemoveItem(&productForDelete)
-
-	//Проверка
-	assert.Equal(t, Cart{Items: make(map[string]CardItem)}, cart)
-
-	//Подготовка
-	cart2 := Cart{Items: make(map[string]CardItem, 1)}
-
-	productForDelete2 := Item{
-		name:     "Milk",
-		weight:   1000,
-		priceRUB: 70,
-	}
-
-	//Выполнение
-	cart2.RemoveItem(&productForDelete2)
-
-	//Проверка
-	assert.Equal(t, Cart{Items: make(map[string]CardItem)}, cart2)
-
-	//Подготовка
-	cart3 := Cart{Items: make(map[string]CardItem, 1)}
-
-	productForDelete3 := Item{
-		name:     "Milk",
-		weight:   1000,
-		priceRUB: 70,
-	}
-	cart3.Items["Milk"] = CardItem{
-		item:      &productForDelete3,
-		weightSum: 2000,
-		priceSum:  140,
-		countItem: 2,
-	}
-
-	expectedCart := Cart{Items: make(map[string]CardItem)}
-
-	expectedCart.Items["Milk"] = CardItem{
-		item:      &productForDelete3,
-		weightSum: 1000,
-		priceSum:  70,
-		countItem: 1,
-	}
-	//Выполнение
-	cart3.RemoveItem(&productForDelete3)
-
-	//Проверка
-	assert.Equal(t, expectedCart, cart3)
 }
